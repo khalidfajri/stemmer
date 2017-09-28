8ab1a75ee2d1b121db912add18384aafd4e283ff
Khalid Fajri <khalidfajri@outlook.com>
2017-09-28 23:32:42 +0700

    update tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -1,11 +1,15 @@
+import re
+
 TEXT = """Kami belajar tanpa lelah.
-Kami tidur tak teratur.
+Kami& tidur tak teratur.
 Kami kuliah dengan giat.
-Kami korbankan masa muda.
+Kami korbankan masa#muda.
 Itu karena kami ingin kelak istri dan anak kami bahagia."""
 
 def tokenize(text):
     text = text.lower()
+    text = re.sub(r'[^a-z0-9 -]', ' ', text, flags = re.IGNORECASE|re.MULTILINE) # fungsinya mengubah selain karakter tsb menjadi spasi
+    text = re.sub(r'( +)', ' ', text, flags = re.IGNORECASE|re.MULTILINE) # fungsinya jika ada lebih dari 1 spasi, maka dijadikan satu
     tokens = text.split(" ")
     return tokens
 
